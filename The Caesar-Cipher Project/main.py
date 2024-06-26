d8f30cf837f0011af54b645e41566c929082c95e
Dharmikkanani12 <152086922+Dharmikkanani12@users.noreply.github.com>
2024-06-26 23:20:43 +0000

    Run the main.py file in this folder to show demo.

--- The Caesar-Cipher Project/main.py
+++ The Caesar-Cipher Project/main.py
@@ -1,3 +1,4 @@
+#Description: This project is to encrypt and decrypt the message"""
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
